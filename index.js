699159431c3ad405ee189f9de1ba07555bdce174
Arendra Lodhi <88219671+Arendra1@users.noreply.github.com>
2022-10-14 21:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors';
+import connectDb  from './config/connectdb.js'
+import userRoutes from './routes/userRoutes.js'
+import quizRoutes from './routes/quizRoutes.js'
+import idRoutes from './routes/incemenet.js'
 
 
-app.get('/' , (req , res))
+const app = express()
+const port = process.env.PORT || 8080
+const DATABASE_URL = process.env.DATABASE_URL
 
-app.listen(port , 8000)
+// CORS Policy
+app.use(cors()) 
+
+
+// Database Connection
+connectDb(DATABASE_URL)
+
+// JSON
+app.use(express.json())
+
+
+// Load Routes
+app.use('/api/user' , userRoutes)
+app.use('/api/quiz', quizRoutes)
+app.use('/api/id', idRoutes)
+
+
+app.listen(port , () =>{
+    console.log(`Server Listening at http://localhost:${port}`)
+})
